b33bc3141dd4c851722d2f58f5c6d42f3e8440c8
JanCech9 <honza.cech14@gmail.com>
2026-05-25 14:23:23 +0200

    refact: default opening hours edit

--- src/constants.js
+++ src/constants.js
@@ -6,8 +6,8 @@ export const MONTHS = [
   "July","August","September","October","November","December"
 ];
 
-export const DEFAULT_WEEKDAY = { start: "08:00", end: "17:00" };
-export const DEFAULT_WEEKEND = { start: "09:00", end: "16:00" };
+export const DEFAULT_WEEKDAY = { start: "06:45", end: "18:00" };
+export const DEFAULT_WEEKEND = { start: "09:00", end: "17:00" };
 
 export const STAFF_COLORS = [
   "#b85c20","#2e8c5a","#b83030","#2060a8","#7040a8","#a87c10",
